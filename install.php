dc12d0ecb4db11444fa9c78840032b071580af1a
fran6t <trautmann@wse.fr>
2014-03-25 10:36:50 +0100

    Prise en compte d'un cache
    Le plugin cherche maintenant les images sur le site original
    Pour cela il faut pour l'instant remplir à la main la table MagFontions_param
    (uniquement pour les sites qui ne publient pas leurs flux complets)
    
    Exemple pour le site www.presse-citron.net l'auteur ne publie pas
    dans son flux de données images. En renseignant la table MagFonctions_param
    comme ci-dessous nous indiquons au plugin d'aller chercher la premiere image
    representative dans la page originale en allant l'extraire de la balise
    div class post-content
    
    On renseigne donc la table avec l'id du site chez moi 4 c'est facile
    de trouver l'id il suffit de regarder l'url et de regarder feed=4
    
    Cela nous donne donc
    id=4
    tag=div
    idouclass=class
    validouclass=post-content
    
    les champs website et remarque ne servent pas encore

--- install.php
+++ install.php
@@ -1,16 +1,44 @@
 <?php
 /* 
-	Le code contenu dans cette page ne sera éxecuté qu'à l'activation du plugin 
-	Vous pouvez donc l'utiliser pour créer des tables MySQL, des dossiers, ou executer une action
-	qui ne doit se lancer qu'à l'installation ex :
-	
-	mysql_query('
-		CREATE TABLE IF NOT EXISTS `'.MYSQL_PREFIX.'plugin_squelette` (
-		  `id` int(11) NOT NULL AUTO_INCREMENT,
-		  `squelette_champ1` int(11) NOT NULL,
-		  PRIMARY KEY (`id`)
-		) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+	* Nous avons besoins de deux tables :
+	* 
+	* La table MagFontions_param :
+	* qui doit permettre de faire connaitre 
+	* entre autre le tag permettant d'aller chercher une image representative
+	* d'un billet sur le site concerné par un flux exemple le site pressecitron.net
+	* fournit un flux expurgé des images comme il s'agit d'un wordpress il va suffir 
+	* de renseigner que l'image fait partie d'une dis ayant la class post-content et 
+	* nous extrayons l'image d'illustration de cette partie
+	* 
+	* La table MagFonctions_cache :
+	* il s'agit de ne pas faire de recherche de taille d'image sur un site distant 
+	* dès lors que cette recherche a déjà été faite une fois. 
+*/	
+
+	mysql_query('			
+		CREATE TABLE IF NOT EXISTS `'.MYSQL_PREFIX.'MagFonctions_cache` (
+			`id` int(11) NOT NULL,
+			`feed` int(11) NOT NULL,
+			`urlimage` char(250) CHARACTER SET utf8 NOT NULL,
+			`imgwidth` int(11) NOT NULL,
+			`imgheight` int(11) NOT NULL,
+			PRIMARY KEY (`id`)
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 	');
 
-*/
+
+
+	mysql_query('
+		CREATE TABLE IF NOT EXISTS `'.MYSQL_PREFIX.'Fonctions_param` (
+			`id` int(11) NOT NULL,
+			`tag` varchar(50) CHARACTER SET utf8 NOT NULL,
+			`idouclass` varchar(50) CHARACTER SET utf8 NOT NULL,
+			`validouclass` varchar(50) CHARACTER SET utf8 NOT NULL,
+			`website` text CHARACTER SET utf8 NOT NULL,
+			`Remarque` varchar(250) CHARACTER SET utf8 NOT NULL,
+			UNIQUE KEY `id` (`id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+	');
+
 ?>
--- uninstall.php
+++ uninstall.php
@@ -3,8 +3,8 @@
 	Le code contenu dans cette page ne sera éxecuté qu'à la désactivation du plugin 
 	Vous pouvez donc l'utiliser pour supprimer des tables MySQL, des dossiers, ou executer une action
 	qui ne doit se lancer qu'à la désinstallation ex :
-	
-	mysql_query('DROP TABLE '.MYSQL_PREFIX.'plugin_squelette');
-
 */
+	mysql_query('DROP TABLE '.MYSQL_PREFIX.'MagFonctions_cache');
+	mysql_query('DROP TABLE '.MYSQL_PREFIX.'MagFonctions_param');
+
 ?>
